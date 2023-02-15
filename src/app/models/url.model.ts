d20e98b254deff353b19f20bff5f5f5737c7314f
Mihails Dudarevs <deadcatofdoom@googlemail.com>
2023-02-15 20:16:59 +0200

    initial commit

---
+++ src/app/models/url.model.ts
@@ -0,0 +1,15 @@
+export interface UrlModel {
+  url: string;
+  expiration: Expiration;
+}
+
+export interface Expiration {
+  unit: string;
+  amount: number;
+}
+
+export interface UrlApiResponse {
+  url: string;
+  expires: string
+}
+
